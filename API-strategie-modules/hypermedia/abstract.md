7f45d82db303d0b759a2ad4103a58bce389f18a9
Martin van der Plas <martinvanderplas@gmail.com>
2023-06-06 16:34:56 +0200

    added hypermedia module

---
+++ API-strategie-modules/hypermedia/abstract.md
@@ -0,0 +1,3 @@
+Deze hypermedia module is gebaseerd op het hypermedia hoofdstuk van het [NL API Strategie extensies document](https://docs.geostandaarden.nl/api/API-Strategie-ext/#hypermedia)
+
+<p class="note">The working group has indicated this module to be stable.</p>
---
+++ API-strategie-modules/hypermedia/js/config.js
@@ -0,0 +1,51 @@
+const documentConfig = {
+    //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
+    specStatus: "WV",
+    //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
+    specType: "HR",
+    pubDomain: "api",
+    shortName: "hypermedia",
+    publishDate: "2023-06-05",
+    publishVersion: "1.0.0",
+    previousPublishVersion: "0.0.0",
+    //  previousPublishDate: "(none)",
+    //  previousMaturity: "WV",
+    editors:
+        [
+            {
+                name: "Logius Standaarden",
+                company: "Logius",
+                companyURL: "https://github.com/Logius-standaarden",
+            }
+        ],
+    authors:
+        [
+            {
+                name: "Logius Standaarden",
+                company: "Logius",
+                companyURL: "https://github.com/Logius-standaarden",
+            }
+        ],
+    github: "https://github.com/Geonovum/KP-APIs",
+
+
+    // Controls if linked "§" section markers are added to a document
+    addSectionLinks: true,
+
+    // this parameter will add the tag_name of the latest release to the document Title
+    // only set this parameter when a release has been set
+    nl_addReleaseTagTitle: true,
+
+    // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
+    // so that figures can be linked are be a part of table of figures
+    nl_markdownEmbedImageInFigure: true,
+    
+    localBiblio: {
+                "HAL": {
+          href: "http://stateless.co/hal_specification.html",
+          title: "HAL - Hypertext Application Language",
+          authors: ["Mike Kelly"],
+          date: " 2013-09-18",
+        },
+    }
+}
