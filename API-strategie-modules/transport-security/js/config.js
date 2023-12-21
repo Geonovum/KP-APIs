984edcb7fc7085bb9f2892d91b1bdbae6b74afb3
Arnoud de Boer <a.deboer@geonovum.nl>
2023-12-21 08:53:13 +0100

    omzetten naar Geonovum Respec

--- API-strategie-modules/transport-security/js/config.js
+++ API-strategie-modules/transport-security/js/config.js
@@ -1,17 +1,48 @@
-const documentConfig = {
-    //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
-    specStatus: "VV",
-    //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
-    specType: "HR",
-    pubDomain: "api",
-    shortName: "transport-security",
-//    publishDate: "2023-05-24",
-    publishVersion: "1.0.0",
-    previousPublishVersion: "(none)",
-    previousPublishDate: "(none)",
-    //  previousMaturity: "WV",
-    edDraftURI: "https://geonovum.github.io/KP-APIs/API-strategie-modules/transport-security/",    
-    editors:
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  // title is verplicht! Neem hier de titel van het document op ----------------------
+  title: "Transport Security Module",
+  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+  specStatus: "vv",                   // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "basis",            // Basis Document
+  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+  //specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  specType: "HR",                     // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
+  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
+  pubDomain: "api",
+  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //licence: "cc0",                 // Public Domain Dedication
+  licence: "cc-by",                 // Attribution, met bronvermelding
+  //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+  shortName: "transport-security",
+  edDraftURI: "https://geonovum.github.io/KP-APIs/API-strategie-modules/transport-security/",  
+  //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
+	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
+
+  //-- publishDate is verplicht -------------------------------------------------------
+  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  //publishDate: "2023-03-28",
+  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
+  publishVersion: "1.0.0",
+  //previousVersion: "0.0.1",
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
+  //previousPublishDate: "2014-05-01",
+  //previousMaturity: "CV",
+  //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
+  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
+  editors:
         [
             {
                 name: "Kennisplatform API's",
@@ -27,10 +58,9 @@ const documentConfig = {
                 companyURL: "https://github.com/Geonovum/KP-APIs",
             }
         ],
-    github: "https://github.com/Geonovum/KP-APIs",
-
-
-    // Controls if linked "§" section markers are added to a document
+  //neem hier de URL van de github repository op waar het respec document in staat
+  github: "https://github.com/Geonovum/KP-APIs",
+  // Controls if linked "§" section markers are added to a document
     addSectionLinks: true,
 
     localBiblio: {
@@ -40,5 +70,13 @@ const documentConfig = {
           authors: ["NCSC"],
           date: "Januari 2021"
         }
-    }
-}
+    }  
+  
+  // Create PDF and link to file in header (optional):
+  //alternateFormats: [
+  //    {
+  //        label: "pdf",
+  //       uri: "template.pdf",
+  //    },
+  // ],
+};
