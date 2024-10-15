622cb9a833e05d94318ea867b7aca6a30b2f47aa
Frank Terpstra <frank@frankterpstra.com>
2024-10-15 13:35:44 +0200

    Update config.js

--- API-strategie-modules/orchestration/js/config.js
+++ API-strategie-modules/orchestration/js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Naming Conventions",
+  title: "Orchestration",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
